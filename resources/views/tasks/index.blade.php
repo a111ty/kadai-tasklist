f3db162bb8a99993b29b908131f27d3c69e10a35
ai.toyoda <a111.asai.88@gmail.com>
2019-08-21 07:31:13 +0000

    update tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,10 +13,10 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($tasks as $tasks)
+                @foreach ($tasks as $task)
                 <tr>
-                    <td>{!! link_to_route('tasks.show', $tasks->id, ['id' => $tasks->id]) !!}</td>
-                    <td>{{ $tasks->content }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
